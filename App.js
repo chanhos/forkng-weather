e341eecd75eece300900d09f9fcf6aeca048e870
정찬호 <lovgone@naver.com>
2019-11-12 19:47:34 +0900

    2nd commit

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { Component } from 'react';
+import { Alert } from "react-native" ; 
+import Loading from './Loading' ;
+import * as Location from 'expo-location' ;  
+import axios from 'axios' ; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!!</Text>
-    </View>
-  );
+
+const API_KEY = "ee6701ef2752bf911a0b6a4ec88d9314" ; 
+
+export default class  extends Component {
+  state = {
+    isLoading : true , 
+
+  }
+
+  getWeather = async(latitude , longitude ) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    console.log (data) ; 
+  };
+
+  getLocation = async() => {
+    try{
+      
+      const response = await Location.requestPermissionsAsync();
+      const {
+        coords : { latitude , longitude }
+      } = await Location.getCurrentPositionAsync();
+      
+      console.log(latitude, longitude) ; 
+      this.getWeather(latitude , longitude ) ; 
+      this.setState( { isLoading : false }) ;
+      // Send to API and get Weather
+      
+      //console.log(coords.latitude , coords.longitude) ; 
+    } catch(error) {
+      Alert.alert("Cant't find you" ,"So sad") ; 
+    }
+    
+  };
+  
+  componentDidMount(){
+    this.getLocation();
+  }
+  
+  render(){
+    const { isLoading } = this.state ;  
+
+    return isLoading ? <Loading />  : null ; 
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
